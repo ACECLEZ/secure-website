af9486b461d7a5e73e61b103ced018b0ed178b93
ACECLEZ <121684067+ACECLEZ@users.noreply.github.com>
2023-03-24 21:58:16 +0800

    Create location-block.js

---
+++ location-block.js
@@ -0,0 +1,16 @@
+function checkLocation() {
+      fetch('https://api.ipify.org/?format=json')
+        .then(response => response.json())
+        .then(data => {
+          var userIP = data.ip;
+          fetch(`https://ipapi.co/${userIP}/json/`)
+            .then(response => response.json())
+            .then(data => {
+              // Check if the user is located in Selected country
+              if (data.country === 'replace_country_code_here') {
+                window.location.href = 'error.html';
+              }
+            });
+        });
+}
+/*@ACECLEZ*/
